96a88e56a173752499c48f351b44ce69b45291b9
iblessme <vldikk12@gmail.com>
2022-05-05 09:07:54 +0300

    Initial Commit

---
+++ EvaLoginScreen/EvaLoginScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  EvaLoginScreenApp.swift
+//  EvaLoginScreen
+//
+//  Created by iBlessme on 05.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct EvaLoginScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
